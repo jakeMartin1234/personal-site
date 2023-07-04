e71052e9d2471e8690d460f5b78213f761456854
Jake Martin <jakemartin@berkeley.edu>
2023-07-04 01:35:54 -0700

    added custom buttons

--- src/components/Template/Carousel.js
+++ src/components/Template/Carousel.js
@@ -1,40 +1,75 @@
 import React from 'react';
 import Slider from 'react-slick';
+import PropTypes from 'prop-types';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import './carousel.css';
 
 const photos = [
   'fijiFish.jpg',
   'giantSequoia.jpeg',
   'hawaii.jpg',
   'bayAreaCrab.jpg',
+  'elephant.jpg',
+  'sunset.jpg',
 ];
 
 const JakeCarousel = () => {
   const settings = {
     infinite: true,
-    speed: 500,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
     slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
+    slidesToScroll: 2,
     variableWidth: true,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
   };
   return (
-    <Slider {...settings}>
-      {photos.map((photo) => (
-        <div>
-          <img
-            src={`/images/carousel/${photo}`}
-            alt="Jake Martin"
-            style={{
-              width: 'auto',
-              height: '500px',
-            }}
-          />
-        </div>
-      ))}
-    </Slider>
+    <div className="slider-container">
+      <Slider {...settings}>
+        {photos.map((photo) => (
+          <div>
+            <img
+              src={`/images/carousel/${photo}`}
+              alt={photo}
+              style={{
+                width: 'auto',
+                height: '550px',
+              }}
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
+};
+
+const CustomPrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <button className="custom-button prev-button" onClick={onClick} type="button">
+      Previous
+    </button>
+  );
+};
+
+const CustomNextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <button className="custom-button next-button" onClick={onClick} type="button">
+      Next
+    </button>
+  );
+};
+
+CustomPrevArrow.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+CustomNextArrow.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export default JakeCarousel;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 import JakeCarousel from '../components/Template/Carousel';
@@ -6,12 +7,18 @@ import ContactIcons from '../components/Contact/ContactIcons';
 import './contactInfo.css';
 
 const Index = () => (
-  <Main description={"Jake Martin's personal website"}>
+  <Main description={"Jake Martin's personal website"} fullPage>
     <article className="post" id="index">
       <p>
         Hi, I am Jake, a UC Berkeley graduate with a degree in Computer Science.
         I&apos;m passionate about web development
         through which I have experience using frameworks such as React and Node.
+        Please feel free to read more <Link to="/about">about me</Link>,
+        or you can check out my {' '}
+        <Link to="/resume">resume</Link>, {' '}
+        <Link to="/projects">projects</Link>, {' '}
+        view <Link to="/stats">site statistics</Link>, {' '}
+        or <Link to="/contact">contact</Link> me.
       </p>
       <div className="contact-info">
         <a href="mailto:jakemartinaus@gmail.com">jakemartinaus@gmail.com</a>
